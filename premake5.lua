4fedfdb2261840d07d37c85e3673d84d7287b24b
SeokwonPark <rty3394@naver.com>
2023-10-02 19:52:50 +0900

    Maths

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Steins/vendor/GLFW/include"
 IncludeDir["Glad"] = "Steins/vendor/Glad/include"
 IncludeDir["ImGui"] = "Steins/vendor/imgui"
+IncludeDir["glm"] = "Steins/vendor/glm"
 
 include "Steins/vendor/GLFW"
 include "Steins/vendor/Glad"
@@ -37,7 +38,9 @@ project "Steins"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -46,7 +49,8 @@ project "Steins"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -106,7 +110,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Steins/vendor/spdlog/include",
-		"Steins/src"
+		"Steins/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
