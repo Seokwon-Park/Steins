cf981d27ecb0bc450de934d49569e98d3e5b7c64
SeokwonPark <rty3394@naver.com>
2023-10-17 04:06:51 +0900

    Drawing Component UI

--- Steins/src/Platform/DirectX11/D3D11Context.cpp
+++ Steins/src/Platform/DirectX11/D3D11Context.cpp
@@ -233,14 +233,14 @@ namespace Steins
 		D3D11_BLEND_DESC blendDesc;
 		ZeroMemory(&blendDesc, sizeof(blendDesc));
 		blendDesc.AlphaToCoverageEnable = false;
-		blendDesc.IndependentBlendEnable = true;
+		blendDesc.IndependentBlendEnable = false;
 		blendDesc.RenderTarget[0].BlendEnable = true;
 		blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
 		blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 		blendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
-		blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
-		blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
-		blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_MAX;
+		blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA;
+		blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
+		blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
 		blendDesc.RenderTarget[0].RenderTargetWriteMask	= D3D11_COLOR_WRITE_ENABLE_ALL;
 		m_D3DDevice->CreateBlendState(&blendDesc, m_BlendState.GetAddressOf());
 
--- SteinsGate/src/EditorLayer.cpp
+++ SteinsGate/src/EditorLayer.cpp
@@ -37,12 +37,12 @@ namespace Steins
 
 		auto& transform = m_SquareEntity2.GetComponent<TransformComponent>().Transform;
 		transform[3][0] = 0.5f;
-		transform[3][2] = -0.0f;
+		transform[3][2] = -0.1f;
 		
-		m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
+		m_CameraEntity = m_ActiveScene->CreateEntity("Camera A");
 		m_CameraEntity.AddComponent<CameraComponent>();
 				
-		m_SecondCamera = m_ActiveScene->CreateEntity("Clip-Space Entity");
+		m_SecondCamera = m_ActiveScene->CreateEntity("Camera B");
 		auto& cc = m_SecondCamera.AddComponent<CameraComponent>();
 		cc.Primary = false;
 
@@ -208,35 +208,6 @@ namespace Steins
 		ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
 		ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
 
-		if (m_SquareEntity)
-		{
-			ImGui::Separator();
-			auto& tag = m_SquareEntity.GetComponent<TagComponent>().Tag;
-			ImGui::Text("%s", tag.c_str());
-
-			auto& squareColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
-			ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
-
-			auto& squareColor2 = m_SquareEntity2.GetComponent<SpriteRendererComponent>().Color;
-			ImGui::ColorEdit4("Square Color2", glm::value_ptr(squareColor2));
-		}
-
-		ImGui::DragFloat3("Camera Transform", glm::value_ptr(m_CameraEntity.GetComponent<TransformComponent>().Transform[3]));
-		if (ImGui::Checkbox("Camera A", &m_PrimaryCamera))
-		{
-			m_CameraEntity.GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
-			m_SecondCamera.GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
-		}
-
-		{
-			auto& camera = m_SecondCamera.GetComponent<CameraComponent>().Camera;
-			float orthoSize = camera.GetOrthographicSize();
-			if(ImGui::DragFloat("Second Camera Ortho Size", &orthoSize))
-			{
-				camera.SetOrthographicSize(orthoSize);
-			}
-		}
-
 		ImGui::End();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0,0 });
@@ -251,8 +222,8 @@ namespace Steins
 		
 		//STS_WARN("Viewport Size: {0}, {1}", viewportPanelSize.x, viewportPanelSize.y);
 		//ImGui::Image((void*)m_CheckerboardTexture->GetSRV(), ImVec2{ 256.0f, 256.0f });
-		//auto textureID = m_Framebuffer->GetSRV();
-		auto textureID = m_Framebuffer->GetColorAttachmentRendererID();
+		auto textureID = m_Framebuffer->GetSRV();
+		//auto textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		//auto textureID = m_CheckerboardTexture->GetSRV();
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x ,m_ViewportSize.y });
 		ImGui::End();
