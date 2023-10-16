a8fd130aab682898c3c2de3eac144187e72d6c3a
SeokwonPark <rty3394@naver.com>
2023-10-16 12:28:53 +0900

    Camera Systems Fin.

--- SteinsGate/src/EditorLayer.cpp
+++ SteinsGate/src/EditorLayer.cpp
@@ -29,9 +29,13 @@ namespace Steins
 		square.AddComponent<SpriteRendererComponent>(glm::vec4{0.0f, 1.0f, 0.0f, 1.0f});
 
 		m_SquareEntity = square;
-
+		
 		m_CameraEntity = m_ActiveScene->CreateEntity("Camera Entity");
 		m_CameraEntity.AddComponent<CameraComponent>(glm::ortho(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
+				
+		m_SecondCamera = m_ActiveScene->CreateEntity("Clip-Space Entity");
+		auto& cc = m_SecondCamera.AddComponent<CameraComponent>(glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f));
+		cc.Primary = false;
 	}
 	void EditorLayer::OnDetach()
 	{
@@ -65,7 +69,7 @@ namespace Steins
 		// Update scene
 
 		//Renderer2D::BeginScene(m_CameraController.GetCamera());
-		/m_ActiveScene->OnUpdate(dt);
+		m_ActiveScene->OnUpdate(dt);
 
 		Renderer2D::EndScene();
 		m_Framebuffer->Unbind();
@@ -164,6 +168,13 @@ namespace Steins
 
 			auto& squareColor = m_SquareEntity.GetComponent<SpriteRendererComponent>().Color;
 			ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
+		}
+
+		ImGui::DragFloat3("Camera Transform", glm::value_ptr(m_CameraEntity.GetComponent<TransformComponent>().Transform[3]));
+		if (ImGui::Checkbox("Camera A", &m_PrimaryCamera))
+		{
+			m_CameraEntity.GetComponent<CameraComponent>().Primary = m_PrimaryCamera;
+			m_SecondCamera.GetComponent<CameraComponent>().Primary = !m_PrimaryCamera;
 		}
 
 		ImGui::End();
